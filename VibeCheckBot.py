c3d3e996954501ce7ba4fea98f1356f247a9b3b9
alexsciuto <59966435+alexsciuto@users.noreply.github.com>
2022-06-25 20:31:38 -0400

    update

--- VibeCheckBot.py
+++ VibeCheckBot.py
@@ -1,11 +1,19 @@
 ## THIS CODE WORKS TO RESPOND TO TWEET 
 
+from doctest import DocFileTest
 import tweepy
 import json
 import requests
 import logging
 import time
 import VibeModel
+import tweepy
+import numpy as np 
+import pandas
+import pygsheets
+import pandas as pd
+import pygsheets
+
 
 
 # This creates a tweepy object which accesses the Twitter V2 API. Authorization is done by passing the tokens & keys.
@@ -17,8 +25,19 @@ client = tweepy.Client(consumer_key="YUwWeeXzpnQaYQYYXbISj7SRx",
 
 
 # This initializes the user @VibeCheckBot
+
 user = client.get_user(username='VibeCheckBot')
 user_id = user.data.id
+# df = pd.DataFrame(columns=[['tweets', 'tweet_id', 'author_id', 'ref_tweet', 'responded']])
+# gc = pygsheets.authorize(service_file='/Users/alexs/DataWithAlex_Github/VibeCheckBot_Final/vibecheckbot-351521-6255c4712ef6.json')
+# sh = gc.open('VibeCheckBot')
+# select the first sheet 
+# # wks = sh[0]
+
+# MASTER_DF = wks.get_as_df()
+#update the first sheet with df, starting at cell B2. 
+
+
 
 
 # This creates a trained Bayes Classifier Model that is able to categorize text (e.g., tweets) into two binary choices:
@@ -27,40 +46,109 @@ vibe = VibeModel.create_vibe_model()
 
 # application.py calls this function every 60s to check if someone mentioned @VibeCheckBot. 
 # If yes, it will respond to the target user's tweet with a vibe check.
-def respondToTweet():
+def respondToTweet(df):
     
     # This obtains user's most recent mentions
-    mentions = client.get_users_mentions(user_id)
+    mentions = client.get_users_mentions(id=user_id, expansions=['referenced_tweets.id', 'author_id'], max_results=100)
+    print("we just got a list of all mentions for user @VibeCheckBot")
+    #mentions = client.get_users_mentions(user_id)
+
     # This places all mentions in a dictionary
     tweets = mentions.data
     
     # This for-loop iterates through all mentioned tweets, and reply's to each of them.
     for tweet in reversed(tweets):
-        
+
+        print('Iterating through tweet: ' + tweet.text)
+
         # This sets the id for the current tweet.
         new_id = tweet.id
+        first_row = df.loc[df['tweets'] == tweet.text]
+        print(first_row)
+
+        # print ('created dataframe DF with Gsheets: ' + first_row.tail(1))
+        # first_row = df_test.iloc[0].tolist()
+        # first_row[1]
 
         # if this phrase is in tweet, respond with a vibe check.
-        if 'vibe check:' in tweet.text:
+        if 'vibe check' in tweet.text and (first_row['responded']=='no').any():
 
             # This prints out tweet in terminal to view how it works.
-            print(tweet.text)
+            print('tweet: ' + tweet.text + ' - has not been responded to. Lets get the vibes! :)')
+            #print(tweet.text)
+            
             # This prints the vibe of the tweet.
             print(VibeModel.get_vibe(tweet.text, vibe=vibe))
             
             try:
 
                 x = VibeModel.get_vibe(tweet.text, vibe=vibe)
-                print("Posted Tweet" + x)
+                
                 client.create_tweet(in_reply_to_tweet_id = new_id, text = x)
+                print("Posted Tweet" + x)
+                
+                df.loc[(df.tweets == tweet.text),'responded']='yes'
+                print("tweet marked as responded")
+
 
             except:
                 print("failed")
                 #logger.info("Already replied to {}".format(mention.id))
+                
+    return df
+
+
+def test():
+    mentions = client.get_users_mentions(id=user_id, expansions=['referenced_tweets.id', 'author_id'] )
+    print(mentions)
+
+
+def get_referenced_tweets(tweets, data_frame):
+
+    length = len(tweets)
+
+    for i in range(length):
+    
+        tweet = tweets.data[i-1]
+
+        if tweet.referenced_tweets is None:
+            print("Nonetype Object: " + tweet.text + ' | ' + 'id Nonetype' + "---- tweet id = " + str(tweet.id))
+        
+        else:
+            tweet_t = tweet.referenced_tweets
+            values = np.array([[tweet.id],[tweet_t[i-1]['id']]])
+            columns = ['tweet_id', 'ref_tweet']
+            temporary_df = pd.DataFrame({'tweet_id': [str(tweet.id)], 'ref_tweet': [str(tweet_t[i-1]['id'])], 'tweets': [str(tweet.text)], 'author_id': [str(tweet.author_id)], 'responded': 'no'})
+            data_frame = pd.concat([data_frame, temporary_df], sort=False)
+            print("Type Object: " + tweet.text + ' | ' + str(tweet_t[i-1]['id']) + "---- tweet id = " + str(tweet.id))
+            temporary_df.tail(10)
+
+    return data_frame
+
+def update_sheets(df_final, wks):
+    wks.set_dataframe(df_final,(1,1))
+
+
+
 
 
 def main():
-    respondToTweet()
+    
+    # tweets = client.get_users_mentions(id=user_id, expansions=['referenced_tweets.id', 'author_id'] )
+    # tweets_d = tweets.data
+
+    # df_referenced =  get_referenced_tweets(tweets, df_referenced)
+    # mentions = client.get_users_mentions(user_id)
+
+    # df_tweets = pd.DataFrame(data=[[tweet.text, tweet.id, tweet.author_id, 'none', 'no'] for tweet in tweets_d], columns=['tweets', 'tweet_id', 'author_id', 'ref_tweet', 'responded'])
+    # df_tweets['author_id'] = df_tweets['author_id'].apply(str)
+    # df_tweets['tweet_id'] = df_tweets['tweet_id'].apply(str)
+    # df_tweets = df_tweets.append(df_referenced, ignore_index=True)
+    # df_tweets = df_tweets.drop_duplicates(subset=["tweet_id"], keep='last')
+    print("main just ran")
+    # respondToTweet()
+    # update_sheets(df_final, wks)
+
 
 
 # __name__
--- application.py
+++ application.py
@@ -1,17 +1,50 @@
-
-
 import flask
 from flask import Flask
 import VibeCheckBot
 import os
+import pygsheets
+import pandas
+from doctest import DocFileTest
+import tweepy
+import json
+import requests
+import logging
+import time
+import VibeModel
+import tweepy
+import numpy as np 
+import pandas
+import pygsheets
+import pandas as pd
+import pygsheets
 
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
+
 def job():
-    VibeCheckBot.respondToTweet()
-    print("Success")
+
+    gc = pygsheets.authorize(service_file='/Users/alexs/DataWithAlex_Github/VibeCheckBot_Final/vibecheckbot-351521-6255c4712ef6.json')
+    sh = gc.open('VibeCheckBot')
+    wks = sh[0]
+    df = wks.get_as_df()
+
+    df['author_id'] = df['author_id'].apply(str)
+    df['tweet_id'] = df['tweet_id'].apply(str)
+    df['tweets'] = df['tweets'].apply(str)
+    df['ref_tweet'] = df['ref_tweet'].apply(str)
+    df['responded'] = df['responded'].apply(str)
+
+    print("just got google sheet")
+
+    #VibeCheckBot.test()
+
+    df1 = VibeCheckBot.respondToTweet(df)
+    wks.set_dataframe(df1,(1,1))
+
+    # VibeCheckBot.update_sheets()
+    # print("Success")
 
 
 scheduler = BackgroundScheduler()
